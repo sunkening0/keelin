e410eaab093a6e7715e676f8419202fdf5012089
sun.kening <sun.kening@ustcinfo.com>
2019-09-10 10:10:18 +0800

    删除代码空行（使用markdown粘贴代码时，存在空行导致格式错乱）

--- src/main/java/com/skn/keelin/task/quartz/TestQuartzTask.java
+++ src/main/java/com/skn/keelin/task/quartz/TestQuartzTask.java
@@ -14,26 +14,18 @@ import com.skn.keelin.task.quartz.service.ScheduleJobManage;
 public class TestQuartzTask implements CommandLineRunner {
 	@Autowired
 	ScheduleJobManage scheduleJobManage;
-	
 	public void run(String... args) throws Exception {
-		System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作<<<<<<<<<<<<<");
-		
+		System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作<<<<<<<<<<<<<");	
 		HashMap<String,Object> map = new HashMap<String, Object>();
-        map.put("name",1);
-        
-        ScheduleJobEntity scheduleJobEntity = new ScheduleJobEntity();
-        
+        map.put("name",1);     
+        ScheduleJobEntity scheduleJobEntity = new ScheduleJobEntity();     
         scheduleJobManage.deleteJob("job", "test");
         scheduleJobManage.saveCronJob(Job.class, "job", "test", "0 * * * * ?", map);
-
         map.put("name",2);
         scheduleJobManage.deleteJob("job2", "test");
         scheduleJobManage.saveCronJob(Job.class, "job2", "test", "10 * * * * ?", map);
-
         map.put("name",3);
         scheduleJobManage.deleteJob("job3", "test2");
         scheduleJobManage.saveCronJob(Job.class, "job3", "test2", "15 * * * * ?", map);
-
-
 	}
 }
